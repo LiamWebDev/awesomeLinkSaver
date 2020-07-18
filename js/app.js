c5516e8cc0af2e8364fa9d6dccd397f99d83b730
Liam <liam.webdev@gmail.com>
2020-07-18 23:30:45 +0100

    added join function

--- js/app.js
+++ js/app.js
@@ -1,39 +1,38 @@
-function setFocusLinkInput(){
-    document.getElementById("link-input").focus();
+function setFocusLinkInput() {
+  document.getElementById("link-input").focus();
 }
 
-function setTagged(button){
-    button.classList.toggle('tagged');
+function setTagged(button) {
+  button.classList.toggle("tagged");
 }
 
-function join(element) {
-    console.log("element: ", element.target);
-    const previousTagText = element.target.previousSibling.textContent;
-    let nextTagText = null;
-    if (element.target.nextSibling) {
-      nextTagText = element.target.nextSibling.textContent;
-    }
-    console.log("previous tag value: ", previousTagText);
-    console.log("next tag value: ", nextTagText);
-  }
+function join(event) {
+  const previousTag = event.target.previousSibling;
+  const nextTag = event.target.nextSibling;
+  let nextTagText = event.target.nextSibling.textContent;
+  previousTag.innerHTML += " " + nextTagText;
+  nextTag.parentNode.removeChild(nextTag);
+  event.target.parentNode.removeChild(event.target);
+}
 
-
-
-const LinkInput = document.querySelector('input#link-input');
+const LinkInput = document.querySelector("input#link-input");
 
 LinkInput.addEventListener("paste", (event) => {
-    let paste = event.clipboardData.getData("text");
-    let re = /\w+/g;
-    regArray = paste.match(re);
-    for (var i = 0; i < regArray.length; i++) {
-      tagDiv = '<div class="tag" onclick="setTagged(this)">' + regArray[i] + '</div>';
-      plusses = '<div class="plus">' + '+' + '</div>';
-      document.getElementById("tags").innerHTML += tagDiv;
+  let paste = event.clipboardData.getData("text");
+  let re = /\w+/g;
+  regArray = paste.match(re);
+  for (var i = 0; i < regArray.length; i++) {
+    tagDiv =
+      '<div class="tag" onclick="setTagged(this)">' + regArray[i] + "</div>";
+    plusses = '<div class="plus">' + "+" + "</div>";
+    document.getElementById("tags").innerHTML += tagDiv;
+    if (i !== regArray.length - 1) {
       document.getElementById("tags").innerHTML += plusses;
     }
-    document.querySelectorAll(".plus").forEach((plusElm) => {
-      plusElm.addEventListener("click", (e) => {
-        join(e);
-      });
+  }
+  document.querySelectorAll(".plus").forEach((plusElm) => {
+    plusElm.addEventListener("click", (e) => {
+      join(e);
     });
   });
+});
