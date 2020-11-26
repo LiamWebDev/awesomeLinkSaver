668f126b92083743bd38bf5cf95296ea7ab4a719
Carlos Loureda <carloslouredaparrado@gmail.com>
2020-11-26 20:21:03 +0100

    feat: Implemented node-sass and some more improvements

--- js/app.js
+++ js/app.js
@@ -145,15 +145,24 @@ function pasteEventListener() {
       Utils.toggleElementVisibility("error-msg", false);
       Utils.toggleFavouriteStatus("true", favIcon);
       titleInput.value = "";
-      const urlDomain = new URL(urlFull).hostname;
-      const urlPath = new URL(urlFull).pathname;
-      createAllTags(urlDomain, urlPath);
-      Utils.toggleElementVisibility("submit-btn", true, "block");
-      Utils.toggleElementVisibility("title-input", true);
-      Utils.toggleElementVisibility("new-tag-input", true);
-      Utils.toggleElementVisibility("fav-img", true);
-      pathIntoTitleInput(urlPath);
-      setFocusLinkInput();
+      try {
+        const urlDomain = new URL(urlFull).hostname;
+        const urlPath = new URL(urlFull).pathname;
+        createAllTags(urlDomain, urlPath);
+        Utils.toggleElementVisibility("submit-btn", true, "block");
+        Utils.toggleElementVisibility("title-input", true);
+        Utils.toggleElementVisibility("new-tag-input", true);
+        Utils.toggleElementVisibility("fav-img", true);
+        pathIntoTitleInput(urlPath);
+        setFocusLinkInput();
+      } catch (error) {
+        let { message } = error;
+        if (message === "Failed to construct 'URL': Invalid URL") {
+          message = "Please provide a proper URL";
+        }
+        console.error("[catched] error : ", error);
+        Utils.alertErrorMsg(`Ups! I didn't like that!: ${message}`);
+      }
     }
   });
 }
@@ -212,6 +221,7 @@ function favIconEventListener() {
   document.querySelectorAll(".favs-icon").forEach((star) => {
     star.addEventListener("click", (e) => {
       e.stopImmediatePropagation();
+      console.log("star: ", star);
       const { favourite } = star.dataset;
       const { id } = star.dataset;
       Utils.toggleFavouriteStatus(favourite, star);
