276fc749fe10844c2567dd7eb8b6dc4f847dccb3
Carlos Loureda Parrado <carloslouredaparrado@gmail.com>
2021-02-04 20:32:42 +0100

    feat: Added input change validation and some UI improvements
    Also added some mixins on sass files for media queries

--- js/app.js
+++ js/app.js
@@ -1,9 +1,65 @@
-// TODO: check to make this work on HTML tags
+// Define main variables
+
+const DOM_ELEMENTS = {
+  form: null,
+  link: null,
+  analyzeBtn: null,
+}
+// let form = null;
+
+const getDOMElements = () => {
+  DOM_ELEMENTS.form = document.getElementById("newUrlForm");
+  DOM_ELEMENTS.link = document.getElementById("link");
+  DOM_ELEMENTS.analyzeBtn = document.getElementById("js-analyze-btn");
+}
+
+function addEventListeners() {
+  // TODO: check to make this work on HTML tags
+  DOM_ELEMENTS.form.addEventListener("submit", (e) => onSubmit(e));
+  DOM_ELEMENTS.link.addEventListener("input", (e) => onLinkInput(e))
+}
+
+const init = () => {
+  getDOMElements()
+  addEventListeners()
+  console.log("App.js launched!")
+}
+
+init();
+
+// OTHER FUNCTION:
 function onSubmit(event) {
   event.preventDefault();
   console.log("on Submit: ", event);
+  const linkValue = DOM_ELEMENTS.link.value;
+  const isValid = isValidURL(linkValue);
+  if (!isValid) {
+    console.log(`The text ${linkValue} is NOT a proper URL`);
+  } else {
+    console.log(`The text ${linkValue} is a proper URL`);
+  }
+
+  // TODO: 1 get the text introduced on the input
+  // TODO: 2 - split into tags 
+  //  TODO: 3 - showing "fields": title and "save".
+  //  Suggest a title?
+  // TODO: 4 - manage the tags. 
+
+
 }
 
-const form = document.getElementById("newUrlForm");
+const onLinkInput = event => {
+  event.preventDefault();
 
-form.addEventListener("submit", (e) => onSubmit(e));
+  const linkValue = DOM_ELEMENTS.link.value;
+  const isValid = isValidURL(linkValue);
+
+  DOM_ELEMENTS.analyzeBtn.disabled = !isValid;
+  DOM_ELEMENTS.link.classList.remove('error');
+  if (!isValid) {
+    DOM_ELEMENTS.link.classList.add('error');
+    console.log(`The text ${linkValue} is NOT a proper URL`);
+  }
+}
+
+
